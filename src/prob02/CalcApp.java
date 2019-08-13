3921cca86d54d060e196b5a5df69338a4414f49f
BIT <BIT@DESKTOP-08BCIT0>
2019-08-13 17:02:17 +0900

    2019-08-13

--- src/prob02/CalcApp.java
+++ src/prob02/CalcApp.java
@@ -13,5 +13,24 @@ public class CalcApp {
 		String operation = scanner.next();
 		
 		/* operation에 따라 4칙 연산 객체를 생성하고 caculate 메서드를 호출합니다. */
+		switch(operation) {
+		case "+" :
+			Add add = new Add();
+			System.out.println(add.calculate(a, b));
+			break;
+		case "-" :
+			Sub sub = new Sub();
+			System.out.println(sub.calculate(a, b));
+			break;	
+		case "*" :
+			Mul mul = new Mul();
+			System.out.println(mul.calculate(a, b));
+			break;
+		case "/" :
+			Div div = new Div();
+			System.out.println(div.calculate(a, b));
+			break;
+		}
+		
 	}
 }
